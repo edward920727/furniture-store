95a568556fcad647f60dd4fa69ee32f4fdd94266
edward920727 <c9grcsyp82@privaterelay.appleid.com>
2026-01-08 12:29:47 +0800

    Update core.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -5,10 +5,11 @@ const f = createUploadthing()
 export const ourFileRouter = {
   productImage: f({ image: { maxFileSize: "4MB", maxFileCount: 10 } })
     .onUploadComplete(async ({ metadata, file }) => {
-      console.log("Upload complete for userId:", metadata?.userId)
+    console.log("Upload complete for userId:", "test-user");
       console.log("file url", file.url)
-      return { uploadedBy: metadata?.userId }
+      return { uploadedBy: "test-user" };
     }),
 } satisfies FileRouter
 
 export type OurFileRouter = typeof ourFileRouter
+
