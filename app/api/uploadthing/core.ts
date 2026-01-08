6308534d1e29342ae29fab417ca2f3435fd9170d
edward920727 <c9grcsyp82@privaterelay.appleid.com>
2026-01-08 13:32:48 +0800

    ix core.ts type error

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -7,7 +7,7 @@ export const ourFileRouter = {
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("Upload complete for userId:", metadata?.userId)
       console.log("file url", file.url)
-      return { uploadedBy: metadata?.userId }
+      return { uploadedBy: (metadata as any)?.userId };
     }),
 } satisfies FileRouter
 
